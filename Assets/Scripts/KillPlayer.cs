19e35c99f0c614618925b4e2f49432b3ca7b2879
Ghamanako <73056948+Ghamanako@users.noreply.github.com>
2022-10-25 15:38:17 +0700

    re collider buaya

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -6,9 +6,7 @@ public class KillPlayer : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            SceneManager.LoadScene("Dead Ending");
-        }
+        if(collision.gameObject.CompareTag("Player"))
+        SceneManager.LoadScene("Dead Ending");
     }
 }
